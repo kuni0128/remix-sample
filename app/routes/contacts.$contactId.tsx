1fd230a79bd19448d31587026faecf6ea74d33b3
kuniaki <k.ishikawa.0128@gmail.com>
2024-04-28 19:05:58 +0900

    fix: validate contact data

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,11 +1,14 @@
-import { json } from "@remix-run/node";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
 import { getContact } from "../data";
+import invariant from "tiny-invariant";
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
+  if (!contact) throw new Response("Not Found", { status: 404 });
   return json({ contact });
 }
 
