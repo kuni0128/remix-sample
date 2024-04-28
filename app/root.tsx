b4967f1672173b1114fc9701a4b3dee40056f601
kuniaki <k.ishikawa.0128@gmail.com>
2024-04-29 07:36:07 +0900

    fix: replace Link to NavLink

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import {
   Link,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -58,7 +59,12 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>{contact.first} {contact.last}</>
                       ) : (
@@ -67,7 +73,7 @@ export default function App() {
                       {contact.favorite ? (
                         <span>*</span>
                       ) : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
