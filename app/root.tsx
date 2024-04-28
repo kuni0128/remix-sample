6e0a65f3950a45d257526602fea22aa691ea376c
kuniaki <k.ishikawa.0128@gmail.com>
2024-04-29 07:46:23 +0900

    feat: add loading style

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 import appStyleHref from "./app.css?url";
 import { createEmptyContact, getContacts } from "./data";
@@ -27,6 +28,7 @@ export const loader = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -84,7 +86,12 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          className={
+            navigation.state === "loading" ? "loading" : ""
+          }
+          id="detail"
+        >
           <Outlet />
         </div>
 
